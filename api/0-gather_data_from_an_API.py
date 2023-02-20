e0a9f40e367dcb5e625e61cc2c4d24e90abb4c71
danielburongu <d.burongu@alustudent.com>
2023-02-20 17:38:39 +0300

    files updates

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,27 +3,37 @@
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python main.py [employee_id]")
-    sys.exit()
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        employee_id = int(sys.argv[1])
 
-employee_id = sys.argv[1]
-url = 'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id
+        # Make the API request
+        response_todos = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+        response_user = requests.get(f'https://jsonplaceholder.typicode.com/users?id={employee_id}')
 
-response = requests.get(url)
-todos = response.json()
+        # Check if the response was successful
+        if response_todos.status_code == 200 and response_user.status_code == 200:
+            todos = response_todos.json()
+            user = response_user.json()[0]
 
-if not todos:
-    print("User not found")
-    sys.exit()
+            # Count the number of completed and total tasks
+            completed_tasks = [todo for todo in todos if todo['completed']]
+            num_completed = len(completed_tasks)
+            num_total = len(todos)
 
-done_tasks = [todo for todo in todos if todo['completed']]
-total_tasks = len(todos)
+            # Print employee name and task progress
+            print(f"Employee {user['name']} is done with tasks({num_completed}/{num_total}):")
 
-employee_name = todos[0]['userId']
+            # Print list of completed tasks
+            for task in completed_tasks:
+                print(f"\t{task['title']}")
 
-print("Employee Name: {}".format(employee_name))
-print("Employee {} is done with tasks({}/{}):".format(employee_name, len(done_tasks), total_tasks))
-
-for task in done_tasks:
-    print("\t {}".format(task['title']))
+            # Check if formatting of tasks 1-11 is as expected
+            for i, task in enumerate(todos[:11]):
+                expected_format = f"Task {i+1} Formatting: OK"
+                task_format = f"Task {i+1} Formatting: {'OK' if len(task['title']) <= 50 else 'Incorrect'}"
+                assert task_format == expected_format, f"Expected: {expected_format}\nGot: {task_format}"
+        else:
+            print("An error occurred while retrieving the data.")
+    else:
+        print("Please enter a valid employee ID as an argument.")
